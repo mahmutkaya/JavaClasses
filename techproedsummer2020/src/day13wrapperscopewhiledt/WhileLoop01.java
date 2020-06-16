fb009a0218bd40b44ebaaba9f67c3f385ceb78b2
mahmut kaya <mahmutkaya@android-b112317571b0773d.localdomain>
2020-06-16 18:55:22 +0200

    day 13 - whrapper, scope, while

---
+++ techproedsummer2020/src/day13wrapperscopewhiledt/WhileLoop01.java
@@ -0,0 +1,19 @@
+package day13wrapperscopewhiledt;
+
+public class WhileLoop01 {
+
+	public static void main(String[] args) {
+		// While Loop is a loop like For Loop
+		// They do same things with different syntax
+
+		// Print the counting numbers from 1 to 10 on the console
+
+		int i = 1;
+
+		while (i <= 10) {
+			System.out.print(i + " ");
+			i++;
+		}
+	}
+
+}
