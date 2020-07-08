a3090407a0306c71665e0b52aa9c3bd8310b1bbe
mahmut kaya <mahmutkaya@android-b112317571b0773d.localdomain>
2020-07-08 19:05:32 +0200

    day 29 - overriding

---
+++ techproedsummer2020/src/day29overridingdt/Dog.java
@@ -0,0 +1,17 @@
+package day29overridingdt;
+
+public class Dog extends Animal {
+
+	public static void main(String[] args) {
+
+	}
+
+	public void sound() {
+		System.out.println("Dogs bark");
+	}
+
+	public void eat() {
+		System.out.println("Eat meat");
+	}
+
+}
---
+++ techproedsummer2020/src/day29overridingdt/MethodSignature.java
@@ -0,0 +1,27 @@
+package day29overridingdt;
+
+public class MethodSignature {
+
+	public static void main(String[] args) {
+
+	}
+
+	/*
+	 * Method Signature contains just method name and parameters. Access modifiers,
+	 * return type, method body or other keywords are not in Method Signature
+	 */
+	public void add() {
+		System.out.println("Addition");
+	}
+
+	public int add(int i) {
+		System.out.println("Addition");
+		return 5;
+	}
+
+	private int add(String str) {
+		System.out.println("Addition");
+		return 5;
+	}
+
+}
